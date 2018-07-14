bb66edd0d433f9032d85ef3e1b79cda77e50294d
Guppy Train <guppytrain.login@gmail.com>
2018-07-14 15:16:14 -0700

    adding to repo

---
+++ bin/setjava.sh
@@ -0,0 +1,45 @@
+#!/bin/sh
+
+
+echo "$0 $1"
+
+echo "JDKDIR: $JDKDIR"
+
+case "$1" in
+        10)
+	    cd $JDKDIR
+	    pwd
+
+	    if [ -L "$JDKDIR/current" ]; then
+	     	echo "Removing current link to `readlink -f $JDKDIR/current`"
+		sudo rm current 
+		sync
+    	    fi
+	   
+	    echo "Setting Java to 10.0.1"
+	    sudo ln -s jdk-10.0.1 current
+            ;;
+         
+        8)
+	    cd $JDKDIR
+	    pwd
+
+	    if [ -L "$JDKDIR/current" ]; then
+	     	echo "Removing current link to `readlink -f $JDKDIR/current`"
+	    	sudo rm current
+		sync
+	    fi	
+
+	    echo "Setting Java to 1.8.0_171"
+	    sudo ln -s jdk1.8.0_171 current
+            ;;
+         
+        *)
+            echo $"Usage: $0 {8|10}"
+            exit 1
+ 
+esac
+
+
+echo "Current symlink: `readlink -f $JDKDIR/current`"
+
