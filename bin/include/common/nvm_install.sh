8e8f7b6be6a22f6215ee8b20a9be04aa5dba7f11
Guppy Train <guppytrain.login@gmail.com>
2018-08-04 08:33:54 -0700

    added check if nvmrc already source

--- bin/include/common/nvm_install.sh
+++ bin/include/common/nvm_install.sh
@@ -6,11 +6,15 @@ if [ ! -d "$HOME/.nvm" ]; then
 	mkdir "$HOME/.nvm"
 fi
 
-export NVM_DIR="$HOME/.nvm" && (
-		git clone https://github.com/creationix/nvm.git "$NVM_DIR"
-    		cd "$NVM_DIR"
-      		git checkout `git describe --abbrev=0 --tags --match "v[0-9]*" $(git rev-list --tags --max-count=1)`
-	) && \. "$NVM_DIR/nvm.sh"
+export NVM_DIR="$HOME/.nvm"
+
+if [ -d "$NVM_DIR" ] && [ -z "$(ls -A $NVM_DIR)" ]; then
+	git clone https://github.com/creationix/nvm.git "$NVM_DIR"
+    	cd "$NVM_DIR"
+      	git checkout `git describe --abbrev=0 --tags --match "v[0-9]*" $(git rev-list --tags --max-count=1)`
+	
+	. "$NVM_DIR/nvm.sh"
+fi
 
 # add nvm.sh to needed files
 
@@ -24,19 +28,25 @@ if [ -f "$nvmrc_file" ]; then
 	echo "Including .nvmrc in profile"
 
 	if [ -f "$HOME/.profile" ]; then
-		printf "\n%s\n%s\n" "# include .nvmrc if it exists" "[[ -f \"$SHARE_DIR/etc/.nvmrc\" ]] && . \"$SHARE_DIR/etc/.nvmrc\"" >> $HOME/.profile
+		if [ -z "$(cat $HOME/.profile | grep '.nvmrc')" ]; then
+			printf "\n%s\n%s\n" "# include .nvmrc if it exists" "[[ -f \"$SHARE_DIR/etc/.nvmrc\" ]] && . \"$SHARE_DIR/etc/.nvmrc\"" >> $HOME/.profile
+		fi
 	fi
 
 	echo "Including .nvmrc in bashrc"
 
 	if [ -f "$HOME/.bashrc" ]; then
-		printf "\n%s\n%s\n" "# include .nvmrc if it exists" "[[ -f \"$SHARE_DIR/etc/.nvmrc\" ]] && . \"$SHARE_DIR/etc/.nvmrc\"" >> $HOME/.bashrc
+		if [ -z "$(cat $HOME/.bashrc | grep '.nvmrc')" ]; then
+			printf "\n%s\n%s\n" "# include .nvmrc if it exists" "[[ -f \"$SHARE_DIR/etc/.nvmrc\" ]] && . \"$SHARE_DIR/etc/.nvmrc\"" >> $HOME/.bashrc
+		fi
 	fi
 
 	echo "Including .nvmrc in zshrc"
 
 	if [ -f "$HOME/.zshrc" ]; then
-		printf "\n%s\n%s\n" "# include .nvmrc if it exists" "[[ -f \"$SHARE_DIR/etc/.nvmrc\" ]] && . \"$SHARE_DIR/etc/.nvmrc\"" >> $HOME/.zshrc
+		if [ -z "$(cat $HOME/.zshrc | grep '.nvmrc')" ]; then
+			printf "\n%s\n%s\n" "# include .nvmrc if it exists" "[[ -f \"$SHARE_DIR/etc/.nvmrc\" ]] && . \"$SHARE_DIR/etc/.nvmrc\"" >> $HOME/.zshrc
+		fi
 	fi
 
 fi
