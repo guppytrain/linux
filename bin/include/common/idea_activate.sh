55e5507ccf25eea42049a323384be5c7fa736f16
Guppy Train <guppytrain.login@gmail.com>
2018-08-08 17:56:32 -0700

    fixing issue where prompt was not running.

--- bin/include/common/idea_activate.sh
+++ bin/include/common/idea_activate.sh
@@ -2,13 +2,26 @@
 
 echo "Starting idea activate..."
 
+# download application key
+if [ ! -f "$HOME/Downloads/$license_file" ] || [ ! -f "$DOWNLOAD_DIR/$license_file" ]; then
+	key_uri="https://account.jetbrains.com/licenses"
+
+	firefox "$key_uri" & > /dev/null 2>&1
+fi
+
+bash -c "read -s -e -p \"Log in to JetBrains account, if necessary.  Press ENTER to continue...\""
+
+license_file="YMYRG6F1LL.txt"
+if [ -f "$HOME/Downloads/$license_file" ]; then
+	mv -f "$HOME/Downloads/$license_file" "$DOWNLOAD_DIR/." 
+fi
+
 # set idea info
 filename="idea.key"
-IDEA_DIR="$HOME/Dropbox/idea"
 dest_dir="$HOME/.IntelliJIdea2018.1/config"
 
 # insert key
-if [ -f "$IDEA_DIR/$filename" ]; then
+if [ -f "$DOWNLOAD_DIR/$license_file" ]; then
 	$SHARE_DIR/bin/idea.sh &
 
 	sleep 10
@@ -16,7 +29,8 @@ if [ -f "$IDEA_DIR/$filename" ]; then
 	echo "About to add key: $filename"
 
 	if [ -d "$dest_dir" ]; then
-		cp "$IDEA_DIR/$filename" "$dest_dir/."
+		# cp "$DOWNLOAD_DIR/$filename" "$dest_dir/."
+		printf "%s\n%s" "<certificate-key>" "$(cat $DOWNLOAD_DIR/$license_file)" > "$dest_dir/$filename"
 	fi
 
 	IDEA_PID="$(ps aux | egrep '/[i]dea.sh$' | awk '{print $2}')"
