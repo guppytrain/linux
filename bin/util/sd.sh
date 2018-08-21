014b2a7345bcc5d0179569bfe62c7b20e8272d0c
Guppy Train <guppytrain.login@gmail.com>
2018-08-21 09:55:00 -0700

    shorten name of docker alias

--- bin/util/sd.sh
+++ bin/util/sd.sh
@@ -24,7 +24,7 @@
 			echo $"sd(sudo docker) $1 $2 $3 $4 $5 $6 $7 $8"
 			sudo docker container ls $2 $3 $4 $5 $6 $7 $8
 			;;
-		clsall)
+		call)
 			echo $"sd(sudo docker) $1 $2 $3 $4 $5 $6 $7 $8"
 			sudo docker container ls --all
 			;;
