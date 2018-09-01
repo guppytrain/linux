0cc6b2bff0b86b90d7f65fe29ed7bec813e180bb
Guppy Train <guppytrain.login@gmail.com>
2018-08-31 22:12:31 -0700

    add call to start docker service

--- bin/include/arch/docker_base_install.sh
+++ bin/include/arch/docker_base_install.sh
@@ -3,3 +3,6 @@
 # install docker
 pinst.sh docker
 
+# ideally this is run in the main install, but this is cos specific because the dbn version autostarts the docker daemon after package install, so keep here
+sudo systemctl start docker
+
