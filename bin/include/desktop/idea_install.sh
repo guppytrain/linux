2419a08bfdeec1b39520703fdf9ae8dc2bf7fa95
Guppy Train <guppytrain.login@gmail.com>
2018-09-03 22:24:10 -0700

    wait til idea archive is ready before proceeding with install, optional

--- bin/include/desktop/idea_install.sh
+++ bin/include/desktop/idea_install.sh
@@ -8,12 +8,37 @@ IDEA_DIR="$HOME/Dropbox/idea"
 dest_dir="/opt"
 # dest_dir="$HOME/tmp"
 
+# get options
+til_ready=""
+
+while getopts "t" o; do
+    case "$o" in
+        t)
+            til_ready=true
+            ;;
+        *)
+            usage
+            ;;
+    esac
+done
+
+echo "# Opts supplied: t=$til_ready"
+
+shift $(( OPTIND-1 ))
+
+[ -n "${til_ready}" ] &&  
+    echo "Waiting for ${filename} to be 'ready'" && 
+    wait_for_file.sh "${IDEA_DIR}/${filename}"
+
 # install idea, if found otherwise quit
 if [ -f "$IDEA_DIR/$filename" ]; then
+    echo "Extracting install dir name..."
+
 	install_dir="$(tar tzf "$IDEA_DIR/$filename" | sed -e 's@/.*@@' | uniq)"
         
 	echo "About to install file: $filename"
-	sudo tar -xzvf "$IDEA_DIR/$filename" -C "$dest_dir"
+	
+    sudo tar -xzvf "$IDEA_DIR/$filename" -C "$dest_dir"
 
 	ln -s "$dest_dir/$install_dir/bin/idea.sh" "$SHARE_DIR/bin/."
 
