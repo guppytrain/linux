5095015b3b1a7d0cf21bc71d165ca2aa1b249f92
Guppy Train <guppytrain.login@gmail.com>
2018-09-04 17:10:55 -0700

    update README;added script to perfor the actual selection of files and checkin comments for git;added alias for selection of files and checkin comments for git;

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # linux related utilities and functions for dev and docker related stuff.
 # separate installs and setup for different situations.
+# utilities for java, nvm, etc.
---
+++ bin/util/git_fzf_commit.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+
+usage() { echo "USAGE: git_fzf_commit.sh -mf [FILE|DIRECTORY . . .]"; exit 1; }
+
+# iterate each option
+# local OPTIND
+
+m=''
+f=''
+
+while getopts "m:f:h" o; do
+    case "$o" in
+        m)
+            m="${OPTARG}"
+            ;;
+        f)
+            f="${OPTARG}"
+            ;;
+        h)
+            usage
+            ;;
+        *)
+            usage
+            ;;
+    esac
+done
+
+echo "# Opts supplied: m=$m, f=$f"
+
+shift $(( OPTIND-1 ))
+    
+echo "# Args supplied: $#"
+
+# print out git status
+git status -s && 
+(
+    # select files to commit with fuzzy search
+    files="$(git status -s | fzf -m --height=35% --layout=reverse --prompt='File(s): ' | awk '{print $2}')"
+
+    # check comments
+    [ -z "${files}" ] && 
+        echo "No files selected. Aborting..." &&
+            sleep 5 &&
+                exit 1
+
+    # select comments to use with fuzzy search
+    comments="$(cat $DEV_DIR/TODO | fzf -m --height=35% --layout=reverse --prompt='Comment(s): ')"
+
+    # check comments
+    [ -z "${comments}" ] && 
+        echo "No comments selected. Aborting..." &&
+            sleep 5 &&
+                exit 1
+
+    # commit
+    printf "%s\nFiles:\n%s\nComments:\n%s\n" "Commiting files and comments" "${files}" "${comments}"
+
+    git commit -m "${comments}" "${files}"
+
+    sleep 3
+
+    # verify latest commit
+    git log -1
+)
