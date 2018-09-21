c72f933b99c6d3adf63b2c22e985b8f3e0ce1b53
Guppy Train <guppytrain.login@gmail.com>
2018-09-21 12:13:56 -0700

    add todo to repo, plus change delimiter in tac_chunk

--- bin/util/extras/tac_chunk.sh
+++ bin/util/extras/tac_chunk.sh
@@ -4,11 +4,12 @@ chunks=()
 while IFS= read -r -d $'\0'; do # the space after IFS= is meaningful
     chunks+=("$REPLY")
     # echo "$REPLY"
-done < <(cat TODO | tr '\n' '=' | sed -n 's/\=\+/\=/gp' | sed -n 's/\=#/\|#/gp' | tr '=' '\n' | tr '|' '\0')
+done < <(cat TODO | tr '\n' '~' | sed -n 's/\~\+/\~/gp' | sed -n 's/\~#/\|#/gp' | tr '~' '\n' | tr '|' '\0')
 
 n=${#chunks[*]}
 
 for (( i = n-1; i >= 0; i-- ))
 do
     echo "${chunks[i]}"
+    echo ""
 done
